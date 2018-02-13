8163a2be88a619a04d4b4ed89c73c5b3a0ba1c3e
Jayden <jy412134@dal.ca>
2018-02-12 23:00:15 -0400

    first commit, java files

---
+++ MyFrame.java
@@ -0,0 +1,98 @@
+import java.awt.Component;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class MyFrame {
+	private JFrame frame = new JFrame();
+	
+	public MyFrame() {
+		menuBuilder();
+	}
+	
+	//builds the main menu
+	public void menuBuilder() {
+		frameInit("Database Menu");
+		
+		JLabel welcome = new JLabel("Welcome to the database!");
+		JButton views = new JButton("Views/Stored Procedures");
+		JButton tablesAndEdit = new JButton("Tables/Edit");
+		
+		welcome.setBounds(20, 10, 200, 20);
+		views.setBounds(50, 40, 200, 20);
+		tablesAndEdit.setBounds(50, 80, 200, 20);
+		
+		frame.add(views);
+		frame.add(welcome);
+		frame.add(tablesAndEdit);
+		
+		views.addActionListener(new ActionListener() {
+    			public void actionPerformed(ActionEvent e) {
+    				viewBuilder();
+    			}
+		});
+
+		tablesAndEdit.addActionListener(new ActionListener() {
+	    		public void actionPerformed(ActionEvent e) {
+	    			tableBuilder();
+	    		}
+		});
+		
+		frame.setVisible(true);
+	}
+	
+	public void tableBuilder() {
+		frameInit("Table View");
+		addMenuReturnButton();
+		
+		JPanel tableSelect = new JPanel();
+		JButton btn = new JButton("OK");
+		
+		String choices[] = {"Table 1", "Table 2", "Secret Fancy Table"};
+		JComboBox<String> cb = new JComboBox<String>(choices);
+		
+		btn.setBounds(235, 10, 50, 20);
+		tableSelect.setBounds(60, 2, 175, 50);
+		tableSelect.add(cb);
+		
+		
+		frame.add(btn);
+	    frame.add(tableSelect);
+	    frame.setVisible(true);
+	    
+	    btn.addActionListener(new ActionListener() {
+	    		public void actionPerformed(ActionEvent e) {
+	    			//displayTable
+	    			System.out.println(cb.getSelectedIndex());
+	    		}
+	    });
+	}
+	
+	public void viewBuilder() {
+		frameInit("Views");
+		addMenuReturnButton();
+	    	frame.setVisible(true);
+	}
+	
+	public void frameInit(String name) {
+		frame.setVisible(false);
+		frame = null;
+		frame = new JFrame();
+		frame.setTitle(name);
+		frame.setSize(300,150);
+		frame.setLocation(425,250);
+		frame.setLayout(null);
+	    	frame.setResizable(false);
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	public void addMenuReturnButton() {
+		JButton mainMenu = new JButton("<--");
+		mainMenu.setBounds(10, 10, 50, 20);
+		mainMenu.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				menuBuilder();
+			}
+		});
+		frame.add(mainMenu);
+	}
+}
